51da9b7b1c233d38c4a435fa77cb27a2eefee609
Nahuel Laski <laski@live>
2013-09-23 21:41:26 -0300

    mejorado el mensaje de captura y separados los scripts

---
+++ tp1/graficador.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+import matplotlib.pyplot as plot
+'''
+labels = ipsdst.keys()
+sizes = [ipsdst[key] for key in ipsdst.keys()]
+plot.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True)
+plot.axis('equal')
+plot.show()
+
+ips = [int(ip.split(".")[3]) for ip in ipssrc]
+n, bins, patches = plot.hist(ips)
+plot.xlabel('Apariciones')
+plot.ylabel('Ultimos 8 bits de las IPs')
+#plot.plot(n)
+#plot.subplots_adjust(left=0.15)
+plot.show()
+'''
