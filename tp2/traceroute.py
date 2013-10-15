3e64fda3cd4608d7e34e765b67108a37f9088a42
root <root@bt.foo.org>
2013-10-15 19:00:19 -0300

    traceroute scapy

--- tp2/traceroute.py
+++ tp2/traceroute.py
@@ -1,2 +1,11 @@
 #! /usr/bin/python
 from scapy.all import *
+
+target = "www.google.com"
+ans,unans = sr(IP(dst=target, ttl=(1,25), id=RandShort())/ICMP())
+for snd,rcv in ans:
+	print snd.ttl, rcv.src
+
+print ans.summary()
+
+
